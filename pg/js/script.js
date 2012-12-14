9fa4a730fab41398776378ed5e300e0b12f67502
Hamid Raza <contact@hamidraza.com>
2012-12-14 13:39:49 +0530

    try catch

--- pg/js/script.js
+++ pg/js/script.js
@@ -1,10 +1,14 @@
 function capturePhoto(){
-    navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
+    try{
+		navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
+	}catch(e){
+		console.log(e);
+	}
 }
 function uploadPhoto(data){
 	// this is where you would send the image file to server
 	//output image to screen
-	console.log(data);
+	//console.log(data);
 	cameraPic.src = "data:image/jpeg;base64," + data;
 	navigator.notification.alert(
 		'Your Photo has been uploaded', // message
@@ -13,31 +17,6 @@ function uploadPhoto(data){
 		'OK'                            // buttonName
 	);
 }
-
-
-
-function uploadPhoto(data){
-// this is where you would send the image file to server
-    cameraPic.src = "data:image/jpeg;base64," + data;
-    // Successful upload to the server
-    navigator.notification.alert(
-        'Your Photo has been uploaded',  // message
-        okay,                           // callback
-        'Photo Uploaded',              // title
-        'OK'                          // buttonName
-    );
-    // upload has failed Fail
-    /*
-    if (failedToUpload){
-    navigator.notification.alert(
-        'Your Photo has failed to upload',
-        failedDismissed,
-        'Photo Not Uploaded',
-        'OK'
-        );
-    }
-    */
-}
 function okay(){
-    // Do something
+	//Do something
 }
