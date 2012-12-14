7e9dc89570d48f60fbe4f20d9aef139955dd7b13
Hamid Raza <contact@hamidraza.com>
2012-12-14 13:30:36 +0530

    new files

---
+++ pg/js/script.js
@@ -0,0 +1,43 @@
+function capturePhoto(){
+    navigator.camera.getPicture(uploadPhoto,null,{sourceType:1,quality:60});
+}
+function uploadPhoto(data){
+	// this is where you would send the image file to server
+	//output image to screen
+	console.log(data);
+	cameraPic.src = "data:image/jpeg;base64," + data;
+	navigator.notification.alert(
+		'Your Photo has been uploaded', // message
+		okay,                           // callback
+		'Photo Uploaded',               // title
+		'OK'                            // buttonName
+	);
+}
+
+
+
+function uploadPhoto(data){
+// this is where you would send the image file to server
+    cameraPic.src = "data:image/jpeg;base64," + data;
+    // Successful upload to the server
+    navigator.notification.alert(
+        'Your Photo has been uploaded',  // message
+        okay,                           // callback
+        'Photo Uploaded',              // title
+        'OK'                          // buttonName
+    );
+    // upload has failed Fail
+    /*
+    if (failedToUpload){
+    navigator.notification.alert(
+        'Your Photo has failed to upload',
+        failedDismissed,
+        'Photo Not Uploaded',
+        'OK'
+        );
+    }
+    */
+}
+function okay(){
+    // Do something
+}
